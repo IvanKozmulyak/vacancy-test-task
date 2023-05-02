695d79143ded53f74dae8d63f24681a30bbf09f3
ikozm <ivankozmulyak@gmail.com>
2023-05-02 15:02:55 +0300

    Init commit

---
+++ src/main/java/com/ikozm/vacancytesttask/VacancyTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ikozm.vacancytesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikozm/vacancytesttask/VacancyTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikozm.vacancytesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
